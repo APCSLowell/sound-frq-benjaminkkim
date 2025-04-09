c11077093b03b620ede384508dd8333764f97ac4
benjaminkkim <bekim@s.sfusd.edu>
2025-04-09 11:48:21 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -20,7 +20,7 @@ public class Sound
         samples[i] = limit;
         count++;
       }
-      if(samples[i] > -limit) {
+      if(samples[i] < -limit) {
         samples[i] = -limit;
         count++;
       }
